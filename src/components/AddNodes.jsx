649a2634a4d29d7d21d09e2401c1415a5edbc500
Jade <jade@fungj.net>
2020-04-16 14:57:37 -0700

    display node metadata

--- src/components/AddNodes.jsx
+++ src/components/AddNodes.jsx
@@ -7,9 +7,12 @@ import './AddNodes.css';
 class AddNodesComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { charVal: '' };
+    // helpText filled when button is hovered over
+    this.state = { charVal: '', helpText: '' };
     this.handleCharChange = this.handleCharChange.bind(this);
     this.handleCharAdd = this.handleCharAdd.bind(this);
+    this.handleHover = this.handleHover.bind(this);
+    this.clearHelpText = this.clearHelpText.bind(this);
   }
   handleCharChange (e) {
     this.setState({ charVal: e.target.value });
@@ -18,6 +21,19 @@ class AddNodesComponent extends Component {
     const { addNode } = this.props;
     addNode({ addType: 'char', contents: this.state.charVal });
     this.setState({ charVal: '' });
+  }
+  handleHover (e) {
+    // get description from dataset in element
+    const { meta } = e.target.dataset;
+    if (meta) {
+      // parse JSON from dataset (data is stringified before being put into attribute)
+      const parsedMeta = JSON.parse(meta)
+      // put node metadata in component state
+      this.setState({ meta: parsedMeta });
+    }
+  }
+  clearHelpText () {
+    this.setState({ meta: null });
   }
   render() {
     const { addNode } = this.props;
@@ -29,38 +45,45 @@ class AddNodesComponent extends Component {
     // if we add more nodes in the future, they'll automatically
     // show up here
     const NodeButtons = Object.keys(items).map((k) =>
-      <div key={k} className="node-button">
+      <div 
+        key={k} 
+        className="node-button"
+        onMouseOver={this.handleHover}
+        onMouseOut={this.clearHelpText}
+      >
         {
           (k === "char")
           // we need text input if it's a character node
             ? <div className="input-group">
-              <input 
-                type="text" 
-                className="form-control" 
-                placeholder="char" 
-                onChange={this.handleCharChange}
-                value={this.state.charVal}
-              />
-              <div className="input-group-append">
-                <button 
-                  className="btn btn-outline-success" 
-                  type="button" 
-                  id="button-add-char"
-                  onClick={this.handleCharAdd}
-                >
-                  add
-                </button>
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="char" 
+                  onChange={this.handleCharChange}
+                  value={this.state.charVal}
+                />
+                <div className="input-group-append">
+                  <button 
+                    className="btn btn-outline-success" 
+                    type="button" 
+                    id="button-add-char"
+                    onClick={this.handleCharAdd}
+                    data-meta={ (items[k].meta) ? JSON.stringify(items[k].meta) : null }
+                  >
+                    add
+                  </button>
+                </div>
               </div>
-            </div>
           // otherwise, just display node name
           : <button
             onClick={handleAdd}
             className="btn btn-outline-success"
             data-addtype={k}
+            data-meta={ (items[k].meta) ? JSON.stringify(items[k].meta) : null }
             key={k}
             style={{ marginBottom: '4.2px' }}
           >
-            {k}
+            { (items[k].name) ? items[k].name : k }
           </button>
         }
       </div>
@@ -68,7 +91,20 @@ class AddNodesComponent extends Component {
     // display as button group
     return(
       <div>
-        { NodeButtons }
+        <div>
+          { NodeButtons }
+        </div>
+        <div>
+          {
+            (this.state.meta) 
+              ? Object.keys(this.state.meta).map(k => (
+                <div key={k}>
+                  <code>{k}:</code> {this.state.meta[k]}
+                </div>
+              ))
+            : null
+          }
+        </div>
       </div>
     );
   };
